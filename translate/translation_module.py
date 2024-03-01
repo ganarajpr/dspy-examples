8bda47477b55caa6525fe82e0d9cdcc7b9f75735
Ganaraj Permunda <ganarajpr@gmail.com>
2024-03-01 09:24:16 +0000

    adding cot for better output

--- translate/translation_module.py
+++ translate/translation_module.py
@@ -1,12 +1,11 @@
 import dspy
 from translation_signature import Translation
-
-
+from similarity_metric import similarity
 class TranslationModule(dspy.Module):
     def __init__(self):
         super().__init__()
 
-        self.generate_answer = dspy.Predict(Translation)
+        self.generate_answer = dspy.ChainOfThought(Translation)
     
     def forward(self, sanskrit):
         prediction = self.generate_answer(sanskrit=sanskrit)
